f1b8399e77f575514c95a05f1c30c0d3b24ba154
Florian Schulz <mail@getflourish.com>
2011-01-04 17:23:25 +0100

    Jetzt geht es (zumindest wenn man die Maus benutzt)

--- src/de/fhpotsdam/conscape/Main.java
+++ src/de/fhpotsdam/conscape/Main.java
@@ -14,6 +14,9 @@ import de.fhpotsdam.unfolding.Map;
 import de.fhpotsdam.unfolding.events.EventDispatcher;
 import de.fhpotsdam.unfolding.interactions.MouseHandler;
 import de.fhpotsdam.unfolding.interactions.TuioCursorHandler;
+import de.fhpotsdam.unfolding.mapdisplay.MapDisplayFactory;
+import de.fhpotsdam.unfolding.providers.OpenStreetMap;
+import de.fhpotsdam.unfolding.utils.MapUtils;
 import de.fhpotsdam.unfolding.geo.Location;
 
 public class Main extends PApplet 
@@ -29,23 +32,21 @@ public class Main extends PApplet
 	{
 		size(1000, 750, GLConstants.GLGRAPHICS);
 
-		map = new Map(this, "map1", 50, 100, 900, 550);
-		map.setTweening(true);
+		map = new Map(this);
+		// map = new Map(this, "Berlin-Mitte", 50, 100, 900, 550, true, false, new OpenStreetMap.CloudmadeProvider(MapDisplayFactory.OSM_API_KEY, 23058));
+		map.setTweening(false);
 		
 		// Tuio-Events
 		eventDispatcher = new EventDispatcher();
 		tuioCursorHandler = new TuioCursorHandler(this, map);
 		eventDispatcher.addBroadcaster(tuioCursorHandler);
-
 		// Maus-Events
-		MouseHandler mouseHandler = new MouseHandler(this, map);
-		eventDispatcher.addBroadcaster(mouseHandler);
-		eventDispatcher.register(map, "pan");
+		eventDispatcher = MapUtils.createDefaultEventDispatcher(this, map);
 		
 		// DataLoader holt die Daten und liefert entsprechende Listen zurück
 		dataLoader = new DataLoader();
 		venues = dataLoader.getVenuesFromLastFM();
-		map.zoomAndPanTo(new Location((float) 52.522, (float) 13.405), 20);
+		 map.zoomAndPanTo(new Location((float) 52.522, (float) 13.405), 20);
 		
 	}
 
@@ -55,17 +56,17 @@ public class Main extends PApplet
 		
 		map.draw();
 		
-//		for (Venue v : venues) {
-//			System.out.println(venues.length);
-//			float lat = Float.valueOf(v.geo_lat);
-//			float lon = Float.valueOf(v.geo_long);
-//			Location location = new Location(lat, lon);
-//			float xy[] = map.mapDisplay.getScreenPositionFromLocation(location);
-//			
-//			noStroke();
-//			fill(255, 0, 0);
-//			ellipse(xy[0], xy[1], 10, 10);
-//		}
+		for (Venue v : venues) {
+			System.out.println(venues.length);
+			float lat = Float.valueOf(v.geo_lat);
+			float lon = Float.valueOf(v.geo_long);
+			Location location = new Location(lat, lon);
+			float xy[] = map.mapDisplay.getScreenPositionFromLocation(location);
+			
+			noStroke();
+			fill(255, 0, 0);
+			ellipse(xy[0], xy[1], 10, 10);
+		}
 		
 		// tuioCursorHandler.drawCursors();
 	}
